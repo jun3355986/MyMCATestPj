f30fd5d7761dd038033a2d5cb56c195211f917c6
longjunjie <jun3355986@163.com>
2022-03-23 10:50:41 +0800

    2333

---
+++ src/main/java/com/jun/common/innerclass/Outer.java
@@ -0,0 +1,49 @@
+package com.jun.common.innerclass;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @className: Outer
+ * @description: 内部类的访问范围测试
+ * @author: jdt
+ * @date: 2022/3/23 00:00
+ **/
+@Slf4j
+public class Outer {
+
+    private int outP = 3;
+    private class Inner {
+        private int inP = 5;
+        public void accessOuterP() {
+            log.info("访问外部类成员：{}",  outP);
+        }
+    }
+
+    class Inner3 {
+        private int inP = 5;
+        public void accessOtherInner() {
+            log.info("访问外部类成员：{}",  new Inner().inP);
+        }
+    }
+
+    static class Inner2{
+        public void accessOuter() {
+            log.info("aaaa: {}", new Outer().outP);
+        }
+    }
+
+    public void accessInP() {
+        log.info("访问内部类的private成员: {}", new Inner().inP);
+    }
+    public static void hk() {
+        new Outer().accessInP();
+    }
+    public static void main(String[] args) {
+
+        Outer outer = new Outer();
+        outer.accessInP();
+        Outer.hk();
+
+    }
+
+}
