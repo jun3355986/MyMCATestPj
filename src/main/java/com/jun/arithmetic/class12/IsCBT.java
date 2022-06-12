68753472c29a9149f2349b837731cf7be19b13be
longjunjie <jun3355986@163.com>
2022-06-12 15:43:13 +0800

    判断一树二叉树是否是完全二叉树01

---
+++ src/main/java/com/jun/arithmetic/class12/IsCBT.java
@@ -0,0 +1,25 @@
+package com.jun.arithmetic.class12;
+
+/**
+ * @className: IsCBT
+ * @description: 判断一树二叉树是否是完全二叉树
+ * @author: jdt
+ * @date: 2022/6/10 08:19
+ **/
+public class IsCBT {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static boolean isCBT1 () {
+
+    }
+
+}
