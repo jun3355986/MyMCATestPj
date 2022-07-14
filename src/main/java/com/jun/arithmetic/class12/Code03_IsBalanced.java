74baf724b0c3ba1ec3e7cfe042ebd56257b11196
longjunjie <jun3355986@163.com>
2022-07-14 12:58:38 +0800

    判断是不是平衡二叉树

---
+++ src/main/java/com/jun/arithmetic/class12/Code03_IsBalanced.java
@@ -0,0 +1,23 @@
+package com.jun.arithmetic.class12;
+
+/**
+ * @className: Code03_IsBalanced
+ * @description: TODO 类描述
+ * @author: jdt
+ * @date: 2022/7/14 12:56
+ **/
+public class Code03_IsBalanced {
+
+    public static class Node{
+        public int value;
+        public Node left;
+        public Node right;
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static boolean isBalanced1() {
+
+    }
+}
