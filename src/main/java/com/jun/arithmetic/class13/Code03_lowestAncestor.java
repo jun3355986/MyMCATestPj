80342d779ccd4330739ca4bb5c3886bfaf5f27ee
龙俊杰 <longjunjie@xinpayroll.com>
2022-07-15 17:31:50 +0800

    找出两节点的最近祖先

---
+++ src/main/java/com/jun/arithmetic/class13/Code03_lowestAncestor.java
@@ -0,0 +1,24 @@
+package com.jun.arithmetic.class13;
+
+/**
+ * @decription:
+ * @date: 2022/7/15 15:56
+ * @author: longjunjie@xinpayroll.com
+ * @Since: 最近的公共祖先
+ */
+public class Code03_lowestAncestor {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node lowestAncestor1() {
+        
+    }
+}
