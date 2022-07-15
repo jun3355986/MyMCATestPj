3ddf8c5c74fb48eca535334bbdac281caf158ad6
longjunjie <jun3355986@163.com>
2022-07-15 09:14:33 +0800

    获取二叉树两点最大距离

---
+++ src/main/java/com/jun/arithmetic/class12/Code06_MaxDistance.java
@@ -0,0 +1,30 @@
+package com.jun.arithmetic.class12;
+
+import java.util.ArrayList;
+
+/**
+ * @className: Code06_MaxDistance
+ * @description: TODO 类描述
+ * @author: jdt
+ * @date: 2022/7/15 09:07
+ **/
+public class Code06_MaxDistance {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static int maxDistance1(Node head) {
+
+    }
+
+    public static ArrayList<Node> getPrelist(Node head) {
+
+    }
+
+}
