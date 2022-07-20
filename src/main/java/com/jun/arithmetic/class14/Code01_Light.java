404ef843f7ce05866477e51c5008851e492c94bd
龙俊杰 <longjunjie@xinpayroll.com>
2022-07-20 18:16:53 +0800

    贪心算法路灯问题

---
+++ src/main/java/com/jun/arithmetic/class14/Code01_Light.java
@@ -0,0 +1,34 @@
+package com.jun.arithmetic.class14;
+
+/**
+ * @decription: 路灯问题
+ * @date: 2022/7/20 17:07
+ * @author: longjunjie@xinpayroll.com
+ * @Since:
+ */
+public class Code01_Light {
+
+    public static int leastLight(String road) {
+        char[] str = road.toCharArray();
+        int light = 0;
+        int i = 0;
+        while (i < str.length) {
+            if ( str[i] == 'X') {
+                i++;
+            } else {
+                light++;
+                if (i + 1 == str.length ) {
+                    break;
+                } else  {
+                    if (str[i + 1] == 'X') {
+                        i = i + 2;
+                    } else {
+                        i = i + 3;
+                    }
+                }
+            }
+        }
+        return light;
+    }
+
+}
