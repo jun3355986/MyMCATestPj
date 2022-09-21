a9eda95033f320da064290a1584e01c97d349c51
longjunjie <jun3355986@163.com>
2022-09-21 13:11:53 +0800

    prim最小生成树算法50%

--- src/main/java/com/jun/arithmetic/class16/Prim.java
+++ src/main/java/com/jun/arithmetic/class16/Prim.java
@@ -44,4 +44,45 @@ public class Prim {
 
     }
 
+    public static int prim(int[][] graph) {
+
+        int size = graph.length;
+        int[] distances = new int[size];
+        boolean[] visit = new boolean[size];
+        visit[0] = true;
+        for (int i = 0; i < size; i++) {
+            distances[i] = graph[0][i];
+        }
+
+        int sum = 0;
+        for(int i = 1; i < size; i++) {
+            // 判断每次到各个点距离最近的点是哪个(遍历distance)，并找到最近的那条线，距离是多少
+            int minPath = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for (int j = 0; j < size ; j++ ) {
+                // 被访问过的点不用处理
+                if (!(visit[j]) && distances[j] < minIndex) {
+                    minPath = distances[j];
+                    minIndex = j;
+                }
+            }
+            // 防止森林
+            if (minIndex == -1) {
+                return sum;
+            }
+            visit[minIndex] = true;
+            sum += minPath;
+            for (int j ; j < size; j++) {
+                // 被访问过的点不处理（因为不需要比较），选择前一个集合最小的边对应的点，并判断这个点的nexts到各个点的边是否小于现有的，是就选择，不是就跳过
+                if (!(visit[j]) && distances[j] < graph[minIndex][j] ) {
+                    distances[j] = graph[minIndex][j];
+                }
+            }
+        }
+
+
+        return sum;
+
+    }
+
 }
